179b226fc2d37cfbc653950bad108a8399d55178
Michael Färber <michael.faerber@uibk.ac.at>
2015-04-20 10:50:15 +0200

    Add comments from lecture.

--- 05.ml
+++ 05.ml
@@ -3,12 +3,18 @@ let f = new_recursive_definition num_RECURSION `(f 0 = 0) /\ (!n. f (SUC n) = (S
 
 let oneiter = new_definition `oneiter f x = (if x = 0 then 0 else x + f (x-1))`;;
 
+(* @ is the Hilbert operator *)
+let sum1 = new_definition `SUM1 = @s. s 0 = 0 /\ (!n. s (SUC n) = s n + SUC n)`;;
+
 
 g `!n. f n = (n * (n+1)) DIV 2`;;
 e INDUCT_TAC;;
+
 (* base case *)
+(* here, you could also use ARITH_TAC instead of using NUM_*_CONV manually *)
 e (REWRITE_TAC [f; NUM_ADD_CONV `0 + 1`; NUM_MULT_CONV `0 * 1`; NUM_DIV_CONV `0 DIV 2`]);;
+
 (* step case *)
 e (REWRITE_TAC [f]);;
-e (ASM_REWRITE_TAC[]);;
+e (ASM_REWRITE_TAC[]);;  (* induction hypothesis *)
 e (ARITH_TAC);;
